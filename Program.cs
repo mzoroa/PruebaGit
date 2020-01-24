d9832869c223830d1c38b1e3d27885739a324368
Mari Carmen Zoroa Moreno <marizoro99m@hotmail.com>
2020-01-24 17:16:59 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Version 2:");
+
+            Console.WriteLine("Ingrese un numero a sumar:");
+            int numero1 = Console.Read();
+            Console.WriteLine("Ingrese otro numero a sumar:");
+            int numero2 = Console.Read();
+
+            int resultado = numero1 + numero2;
+
+            Console.WriteLine(string.Format("El resultado es {0}", resultado));
+
+        }
+    }
+}
